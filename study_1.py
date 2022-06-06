4f820ae4ee43e68ecc972db6bb8a60609a6bbb5a
TeenAsher <tanyushashilina1@gmail.com>
2022-06-06 12:40:43 +0300

    study_1 file 2nd commit

--- study_1.py
+++ study_1.py
@@ -2,3 +2,21 @@
 # First file with random pygame practice
 # TeenAsher
 # --------------------------------------
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+
+all_colours = pygame.Surface((4096, 4096), depth=24)
+
+for r in range(256):
+    print(r+1, 'out of 256')
+    x = (r & 15) * 256
+    y = (r >> 4) * 256
+    for g in range(256):
+        for b in range(256):
+            all_colours.set_at((x+g, y+b), (r, g, b))
+
+pygame.image.save(all_colours, 'allcolours.bmp')
+pygame.quit()
