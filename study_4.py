ccf1642d65ef856e537dbb7d8c45cb0110fd128a
TeenAsher <106741447+TeenAsher@users.noreply.github.com>
2022-06-15 19:46:39 +0300

    study_4 file 2nd commit

--- study_4.py
+++ study_4.py
@@ -2,3 +2,35 @@
 # Fourth file with random pygame practice
 # TeenAsher
 # --------------------------------------
+import pygame
+from pygame.locals import *
+import sys
+from random import randint
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480), FULLSCREEN, 32)
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                rand_col = (randint(0, 255), randint(0, 255), randint(0, 255))
+                rand_pos = (randint(0, 639), randint(0, 479))
+                screen.set_at(rand_pos, rand_col)
+
+        if event.type == KEYUP:
+            if event.key == K_a:
+                screen.fill((0, 0, 0))
+
+        pygame.display.update()
