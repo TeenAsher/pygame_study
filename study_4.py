cc49872ac14aa424b46351c970a11286ebf23d20
TeenAsher <106741447+TeenAsher@users.noreply.github.com>
2022-06-15 21:00:05 +0300

    study_4 file 3rd commit

--- study_4.py
+++ study_4.py
@@ -10,6 +10,7 @@ from random import randint
 pygame.init()
 
 screen = pygame.display.set_mode((640, 480), FULLSCREEN, 32)
+screen.fill((35, 10, 10))
 
 while True:
 
@@ -25,12 +26,13 @@ while True:
 
         if event.type == KEYDOWN:
             if event.key == K_a:
-                rand_col = (randint(0, 255), randint(0, 255), randint(0, 255))
-                rand_pos = (randint(0, 639), randint(0, 479))
-                screen.set_at(rand_pos, rand_col)
+                for i in range(200):
+                    rand_col = (randint(0, 255), randint(0, 255), randint(0, 255))
+                    rand_pos = (randint(0, 639), randint(0, 479))
+                    screen.set_at(rand_pos, rand_col)
 
         if event.type == KEYUP:
             if event.key == K_a:
-                screen.fill((0, 0, 0))
+                screen.fill((35, 10, 10))
 
         pygame.display.update()
